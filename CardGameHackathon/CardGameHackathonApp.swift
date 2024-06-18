4c30277686f586d10c566385292b73537b17620a
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-06-18 21:18:09 +0900

    add: first Commit

---
+++ CardGameHackathon/CardGameHackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameHackathonApp.swift
+//  CardGameHackathon
+//
+//  Created by Hina on 2024/06/18.
+//
+
+import SwiftUI
+
+@main
+struct CardGameHackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardGameHackathon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CardGameHackathon
+//
+//  Created by Hina on 2024/06/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
