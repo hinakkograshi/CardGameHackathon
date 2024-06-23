bc9cdf0f782966ccbbff68f4a1eb4897d496e4d6
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-06-24 06:54:30 +0900

    add: nine cards

--- CardGameHackathon/ContentView.swift
+++ CardGameHackathon/ContentView.swift
@@ -7,11 +7,13 @@
 
 import SwiftUI
 
-class CardGameViewModel: ObservableObject {
-    @Published var cards = ["2", "3", "4", "5", "6", "7", "8", "9", "😭"]
-}
+//class CardGameViewModel: ObservableObject {
+//    @Published var cards = ["2", "3", "4", "5", "6", "7", "8", "9", "😭"]
+//}
 
 struct ContentView: View {
+//    @StateObject var viewModel: CardGameViewModel
+    @State var cards = ["2", "3", "4", "5", "6", "7", "8", "9", "logo"]
 
     @State var isFront = false
 
@@ -19,25 +21,36 @@ struct ContentView: View {
             ZStack {
                 Color.green
                     .ignoresSafeArea()
-                VStack {
-                    Button {
-                        if isFront {
-                            isFront = false
-                        } else {
-                            isFront = true
+                LazyVGrid(columns: [
+                    GridItem(.flexible()),
+                    GridItem(.flexible()),
+                    GridItem(.flexible())
+                ],
+                          alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/,
+                          spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/,
+                          pinnedViews: /*@START_MENU_TOKEN@*/[]/*@END_MENU_TOKEN@*/,
+                          content: {
+                    ForEach(cards, id: \.self) { num in
+                        VStack {
+                            Button {
+                                if isFront {
+                                    isFront = false
+                                } else {
+                                    isFront = true
+                                }
+                            } label: {
+                                Flip(isFront: isFront,
+                                     front: {
+                                    Image("card\(num)") // front
+                                },
+                                     back: {
+
+                                    Image("back") // back
+                                })
+                            }
                         }
-                    } label: {
-                        Flip(isFront: isFront,
-                             front: {
-
-                            Image("card2") // front
-                        },
-                             back: {
-
-                            Image("back") // back
-                        })
                     }
-                }
+                })
             }
         }
 }
