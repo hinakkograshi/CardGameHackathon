cf7ca360d16bec8b01c79109886b34f8a1529e4e
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-06-29 12:52:12 +0900

    add: PlusHint ability

---
+++ CardGameHackathon/CardGameError.swift
@@ -0,0 +1,12 @@
+//
+//  CardGameError.swift
+//  CardGameHackathon
+//
+//  Created by Hina on 2024/06/29.
+//
+
+import Foundation
+
+struct CardGameError: Error {
+    let message: String
+}
--- CardGameHackathon/ContentView.swift
+++ CardGameHackathon/ContentView.swift
@@ -13,54 +13,61 @@ struct ContentView: View {
     @State var sum = 0
     @State var showSumView = false
     @State var firstButtonEnable: Bool = true
-    @State var hintText = false
+    @State var secondButtonEnable: Bool = true
     @State var selectedCard = false
     @State var abilityMode = false
     @State var ans: String = ""
-    @State var indexNum = 0
+    @State var plusHint: String = ""
+    @State var plusHintMode = false
+    @State var indexNumBigSmall = 0
+    @State var indexNumPlus = 0
     @AppStorage("Total") var totalScore = 0
 
 
-        var body: some View {
-            ZStack {
-                Color.green
-                    .ignoresSafeArea()
-                VStack {
-                    Text("This Game Score:\(sum)")
-                        .font(.title2)
-                    if hintText == true {
-                        Text("Please chose open card.")
-                            .foregroundStyle(.red)
-                    }
-                    HStack {
-                        Button(action: {
-                            hintText = true
-                            abilityMode = true
+    var body: some View {
+        ZStack {
+            Color.green
+                .ignoresSafeArea()
+            VStack {
+                Text("This Game Score:\(sum)")
+                    .font(.title2)
+                if abilityMode == true, plusHintMode == true {
+                    Text("Please chose open card.")
+                        .foregroundStyle(.red)
+                }
+                HStack {
+                    // First
+                    Button(action: {
+                        abilityMode = true
 
-                        }, label: {
-                                Text("<>")
-                                    .border(.blue)
-                                    .font(.title)
-                        })
-                        .disabled(firstButtonEnable)
-                        .padding(.horizontal, 30)
-                        Button(action: {
+                    }, label: {
+                        Text("<>")
+                            .border(.blue)
+                            .font(.title)
+                    })
+                    .disabled(firstButtonEnable)
+                    .padding(.horizontal, 30)
+                    // Second
+                    Button(action: {
+                        plusHintMode = true
 
-                        }, label: {
-                            Text("+")
-                                .border(.blue)
-                                .font(.title)
-                        })
-                        .padding(.horizontal, 30)
-                        Button(action: {
+                    }, label: {
+                        Text("+")
+                            .border(.blue)
+                            .font(.title)
+                    })
+                    .disabled(secondButtonEnable)
+                    .padding(.horizontal, 30)
+                    // Third
+                    Button(action: {
 
-                        }, label: {
-                            Text("+")
-                                .font(.title)
-                                .border(.blue)
-                        })
-                        .padding(.horizontal, 30)
-                    }
+                    }, label: {
+                        Text("+")
+                            .font(.title)
+                            .border(.blue)
+                    })
+                    .padding(.horizontal, 30)
+                }
                 LazyVGrid(columns: [
                     GridItem(.flexible()),
                     GridItem(.flexible()),
@@ -70,86 +77,107 @@ struct ContentView: View {
                     ForEach(0..<9, id: \.self) { index in
                         VStack {
                             ZStack {
-
-                            Button {
-                                if movies[index] == false, abilityMode == false {
-                                    movies[index] = true
-                                    if cards[index] != "10" {
-                                        sum += Int(cards[index]) ?? 0
-                                    } else {
-                                        showSumView = true
+                                Button {
+                                    // めくる
+                                    if movies[index] == false, abilityMode == false, plusHintMode == false {
+                                        movies[index] = true
+                                        if cards[index] != "10" {
+                                            sum += Int(cards[index]) ?? 0
+                                        } else {
+                                            showSumView = true
+                                        }
                                     }
-                                }
-                                if abilityMode == true, movies[index] == true {
-//                                    let num = Int(cards[index])!
-//                                    print("cards[index]\(num)")
-                                    indexNum = index
-                                    ans = chose(index: index)
-                                    print(ans)
-                                    abilityMode = false
-                                }
-                                //左縦
-                                if movies[0] == true, movies[3] == true, movies[6] == true {
-                                    firstButtonEnable = false
-                                }
-                            } label: {
-                                Flip(isFront: movies[index],
-                                     front: {
-                                    Image("card\(cards[index])") // front
-                                        .resizable()
+                                    // <>
+                                    if abilityMode == true, movies[index] == true {
+                                        indexNumBigSmall = index
+                                        ans = chooseBigSmall(index: index)
+                                        print(ans)
+                                        abilityMode = false
+                                    }
+                                    //左縦
+                                    if movies[0] == true, movies[3] == true, movies[6] == true {
+                                        firstButtonEnable = false
+                                    }
+                                    // Num+
+                                    if plusHintMode == true, movies[index] == false {
+                                        indexNumPlus = index
+                                        plusHint = chooseRandomValue(index: index)
+                                        print(plusHint)
+                                        plusHintMode = false
+                                    }
+                                    if movies[1] == true, movies[4] == true, movies[7] == true {
+                                        secondButtonEnable = false
+                                    }
+                                } label: {
 
-                                },
-                                     back: {
-                                    Image("back") // back
-                                })
-                            }
+                                    Flip(isFront: movies[index],
+                                         front: {
+                                        Image("card\(cards[index])") // front
+                                            .resizable()
+
+                                    },
+                                         back: {
+                                        Image("back") // back
+                                    })
+                                }
                                 if ans == ">" || ans == "<" {
-                                     let _ = print(ans)
-                                    if index == indexNum {
-                                        let _ = print(indexNum)
+                                    if index == indexNumBigSmall {
                                         Text(ans)
                                             .font(.title)
                                             .foregroundStyle(.red)
                                             .border(.red)
                                     }
                                 }
-                        }
+                                if plusHint != "" {
+                                    if index == indexNumPlus {
+                                        Text(plusHint)
+                                            .font(.title)
+                                            .foregroundStyle(.red)
+                                            .border(.red)
+                                    }
+                                }
+                            }
                         }
                     }
                 })
             }
-                .fullScreenCover(isPresented: $showSumView, onDismiss: {
-                    totalScore += sum
-                    reset()
+            .fullScreenCover(isPresented: $showSumView, onDismiss: {
+                totalScore += sum
+                reset()
 
-                }, content: {
-                    ScoreView(sum: $sum)
-                })
-//                .overlay {
-//                    if showSumView == true {
-//                        Color.black.opacity(0.3)
-//                        VStack {
-//                            Text("合計獲得ポイント:\(sum)")
-//                                .font(.title)
-//                        }
-//                        .frame(maxWidth: .infinity, maxHeight: 400)
-//                        .background()
-//                    }
-//                }
-                .padding()
-            }
-            .onAppear(perform: {
-                cards.shuffle()
-                print("cards\(cards)")
+            }, content: {
+                ScoreView(sum: $sum)
             })
+            .padding()
         }
+        .onAppear(perform: {
+            cards.shuffle()
+            print("cards\(cards)")
+        })
+    }
     func reset() {
         movies = Array(repeating: false, count: 9)
         cards.shuffle()
         sum = 0
         print(cards)
     }
-    func chose(index: Int) -> String {
+    //add
+    func chooseRandomValue(index: Int) -> String {
+        let number = Int(cards[index])!
+        var smallNumber = 0
+        if number >= 5 {
+            smallNumber = number - 3
+        } else if number == 2 {
+            smallNumber = 1
+        }else {
+            smallNumber = 2
+        }
+        let randomValue = Int.random(in: smallNumber..<number)
+        let randomString = String(randomValue)
+        let hintString = "\(randomString)+"
+        return hintString
+    }
+    func chooseBigSmall(index: Int) -> String {
         var returnString = ""
         if index == 0 || index == 3 || index == 6 {
             if cards[index] > cards[index+1] {
@@ -157,6 +185,9 @@ struct ContentView: View {
             } else {
                 returnString = "<"
             }
+        } else {
+            print("Alert")
+
         }
         return returnString
     }
